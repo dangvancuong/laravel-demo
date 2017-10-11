8cb4308dd772f4641b3c4c6f68be1e3e20e60afb
dangvancuong196 <dangvancuong196@gmail.com>
2017-10-11 16:56:39 +0700

    webpack.min.js

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -2,20 +2,26 @@
 @section('title')
     shopping cart
 @endsection
+@section('styles')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+@endsection
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6 col-md-4">
-                <div class="thumbnail">
-                    <img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt="...">
-                    <div class="caption">
-                        <h3>Thumbnail label</h3>
-                        <p>...</p>
-                        <p>...</p>
-                        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+    <div id="product">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/quizzes/fast_food_smarts_rmq/650x350_fast_food_smarts_rmq.jpg" alt="...">
+                        <div class="caption">
+                            <h3>Thumbnail label</h3>
+                            <p>...</p>
+                            <p>...</p>
+                            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 @endsection
